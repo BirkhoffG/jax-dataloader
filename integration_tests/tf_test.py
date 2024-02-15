dd99bea5c568c9b19ba73ba60fe1cc31612a0b9d
BirkhoffG <26811230+BirkhoffG@users.noreply.github.com>
2024-02-15 13:16:26 -0500

    Update tf_test.py to use jdl.ArrayDataset and 'tensorflow' backend

--- integration_tests/tf_test.py
+++ integration_tests/tf_test.py
@@ -5,8 +5,8 @@ import tensorflow as tf
 
 
 def test_jax():
-    ds = tf.data.Dataset.from_tensor_slices((tf.ones((10, 3)), tf.ones((10, 3))))
-    dl = jdl.DataLoader(ds, 'jax', batch_size=2)
+    ds = jdl.ArrayDataset(np.ones((10, 3)), np.ones((10, 3)))
+    dl = jdl.DataLoader(ds, 'tensorflow', batch_size=2)
     for x, y in dl:
         z = x + y
         assert isinstance(z, np.ndarray)
