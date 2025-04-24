405ba43e4d772badba6fdcc9e0d498c66c9b18e0
BirkhoffG <26811230+BirkhoffG@users.noreply.github.com>
2025-04-24 16:22:59 -0400

    Add generator tests for DataLoader across integration tests

--- integration_tests/tf_test.py
+++ integration_tests/tf_test.py
@@ -21,3 +21,23 @@ def test_tf():
         z = x + y
         assert isinstance(z, np.ndarray)
 
+def test_generator():
+    ds = jdl.ArrayDataset(np.ones((10, 3)), np.ones((10, 3)))
+
+    g1 = jdl.Generator().manual_seed(123)
+    g2 = tf.random.Generator.from_seed(123)
+
+    # Create two different dataloaders with different generators
+    dl = jdl.DataLoader(ds, 'tensorflow', batch_size=2, generator=g1, shuffle=True)
+    batch = next(iter(dl))
+
+    dl = jdl.DataLoader(ds, 'tensorflow', batch_size=2, generator=g2, shuffle=True)
+    new_batch = next(iter(dl))
+    # Check that batches are equal using tree_map
+    def are_equal(a, b):
+        return np.all(a == b)
+    # Map the equality function over the entire pytree structure
+    equal_elements = jdl.tree_map(are_equal, batch, new_batch)
+    # Check all elements are True
+    all_equal = all(jdl.tree_leaves(equal_elements))
+    assert all_equal
--- integration_tests/torch_test.py
+++ integration_tests/torch_test.py
@@ -3,6 +3,8 @@ import torch
 import numpy as np
 import jax.numpy as jnp
 from torch.utils.data import TensorDataset
+import jax.random as jrand
+import jax.tree_util as jtu
 
 
 def test_jax_ds():
@@ -22,3 +24,30 @@ def test_torch():
         z = x + y
         assert isinstance(z, np.ndarray)
 
+
+def test_generator():
+    ds = jdl.ArrayDataset(jnp.ones((10, 3)), jnp.ones((10, 3)))
+
+    g1 = jdl.Generator()
+    g2 = torch.Generator().manual_seed(jdl.get_config().global_seed)
+
+    # Create two different dataloaders with different generators
+    dl = jdl.DataLoader(ds, 'pytorch', batch_size=2, generator=g1, shuffle=True)
+    batch = next(iter(dl))
+
+    dl = jdl.DataLoader(ds, 'pytorch', batch_size=2, generator=g2, shuffle=True)
+    new_batch = next(iter(dl))
+    
+    # Check that batches are equal using tree_map
+    def are_equal(a, b):
+        return jnp.all(a == b)
+    
+    # Map the equality function over the entire pytree structure
+    equal_elements = jtu.tree_map(are_equal, batch, new_batch)
+    
+    # Check all elements are True
+    all_equal = all(jtu.tree_leaves(equal_elements))
+    assert all_equal
+    
+    # Also verify the tree structures match
+    assert jtu.tree_structure(batch) == jtu.tree_structure(new_batch)
